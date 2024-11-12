6a05c2a149b81e23c7f2c70dc483bcbb4fd10556
Ashour <mohamedashour.0523093@gmail.com>
2024-11-12 12:02:59 +0200

    add folder to branch

---
+++ library/library/Welcoe.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace library
+{
+    /// <summary>
+    /// Interaction logic for Welcoe.xaml
+    /// </summary>
+    public partial class Welcoe : Page
+    {
+        public Welcoe()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            category c=new category();
+            this.NavigationService.Navigate(c);
+        }
+    }
+}
---
+++ library/library/category.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace library
+{
+    /// <summary>
+    /// Interaction logic for category.xaml
+    /// </summary>
+    public partial class category : Page
+    {
+        LibraryDBEntities db=new LibraryDBEntities();
+        public category()
+        {
+            InitializeComponent();
+            dg.ItemsSource = db.Books.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            dg.ItemsSource=db.Books.Where(x=>x.bookName==stxt.Text).ToList();
+        }
+    }
+}
